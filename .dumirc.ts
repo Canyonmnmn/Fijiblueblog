fc845a280510639e21b92abb4ac60d620818dd47
canyon <1225426443@qq.com>
2023-05-19 14:13:28 +0800

    add：自动部署设置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'dumi';
 
-
+const repo = 'Fijiblueblog'
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   themeConfig: {
     name: 'FijiBlue',
     logo: '/avater.svg',
