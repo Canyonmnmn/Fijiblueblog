7e81e59a787593fbe4ef0cbc0959b781f3e75365
canyon <1225426443@qq.com>
2023-05-19 17:55:15 +0800

    增加网站头像

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,8 +4,9 @@ const repo = 'Fijiblueblog'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  favicons:['/avater.svg'],
   themeConfig: {
-    name: 'FijiBlue',
+    name: 'FijiBlue - 个人博客',
     logo: '/avater.svg',
     socialLinks:{
       github:"https://github.com/Canyonmnmn"
